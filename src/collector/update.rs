7ac0f8475a4f2183eeccbea6cafcc05fa3694f63
Eric Hodel <drbrain@segment7.net>
2024-10-07 22:12:08 -0700

    Extract update::Switch to its own file

--- src/collector/update.rs
+++ src/collector/update.rs
@@ -1,31 +1,8 @@
+mod switch;
+
+pub use switch::Switch;
+
 #[derive(Clone)]
 pub enum Update {
     Switch(Switch),
-}
-
-#[derive(Clone)]
-pub struct Switch {
-    receive: Vec<u64>,
-    transmit: Vec<u64>,
-}
-
-impl Switch {
-    pub fn empty() -> Self {
-        Self {
-            receive: vec![],
-            transmit: vec![],
-        }
-    }
-
-    pub fn new(receive: Vec<u64>, transmit: Vec<u64>) -> Self {
-        Self { receive, transmit }
-    }
-
-    pub fn receive(&self) -> &Vec<u64> {
-        &self.receive
-    }
-
-    pub fn transmit(&self) -> &Vec<u64> {
-        &self.transmit
-    }
 }
---
+++ src/collector/update/switch.rs
@@ -0,0 +1,26 @@
+#[derive(Clone)]
+pub struct Switch {
+    receive: Vec<u64>,
+    transmit: Vec<u64>,
+}
+
+impl Switch {
+    pub fn empty() -> Self {
+        Self {
+            receive: vec![],
+            transmit: vec![],
+        }
+    }
+
+    pub fn new(receive: Vec<u64>, transmit: Vec<u64>) -> Self {
+        Self { receive, transmit }
+    }
+
+    pub fn receive(&self) -> &Vec<u64> {
+        &self.receive
+    }
+
+    pub fn transmit(&self) -> &Vec<u64> {
+        &self.transmit
+    }
+}
