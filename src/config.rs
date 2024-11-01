c1ceaa6c2af1be1f842701034c60488d83ff2efc
Eric Hodel <drbrain@segment7.net>
2024-11-01 14:45:39 -0700

    Extract config::Device to its own file

--- src/config.rs
+++ src/config.rs
@@ -1,70 +1,20 @@
+mod device;
+
 use serde::{Deserialize, Serialize};
 
-use crate::device::{self, Switch};
+pub use device::Device;
 
 #[derive(Deserialize, Serialize)]
 pub struct Config {
     devices: Vec<Device>,
 }
 
-impl From<Config> for Vec<device::Device> {
+impl From<Config> for Vec<crate::device::Device> {
     fn from(config: Config) -> Self {
-        config.devices.iter().map(|device| device.into()).collect()
-    }
-}
-
-#[derive(Deserialize, Serialize)]
-enum Device {
-    Switch {
-        address: String,
-        receive: Option<String>,
-        transmit: Option<String>,
-        poe: Option<String>,
-    },
-}
-
-impl From<&Device> for crate::device::Device {
-    fn from(device: &Device) -> Self {
-        match device {
-            Device::Switch {
-                address,
-                receive,
-                transmit,
-                poe,
-            } => {
-                let labels = format!("instance=\"{address}\"");
-
-                let receive_query = if let Some(receive) = receive {
-                    receive.clone()
-                } else {
-                    format!(
-                        "sum(rate(ifHCInOctets{{{}, ifAlias=~\"(Port|SFP) .*\"}}[1m])) by (ifIndex)",
-                        labels
-                    )
-                };
-
-                let transmit_query = if let Some(transmit) = transmit {
-                    transmit.clone()
-                } else {
-                    format!(
-                        "sum(rate(ifHCOutOctets{{{}, ifAlias=~\"(Port|SFP) .*\"}}[1m])) by (ifIndex)",
-                        labels
-                    )
-                };
-
-                let poe_query = if let Some(poe) = poe {
-                    poe.clone()
-                } else {
-                    format!("unpoller_device_port_poe_amperes{{{}}}", labels)
-                };
-
-                device::Device::Switch(Switch::new(
-                    &labels,
-                    &receive_query,
-                    &transmit_query,
-                    &poe_query,
-                ))
-            }
-        }
+        config
+            .devices
+            .into_iter()
+            .map(|device| device.into())
+            .collect()
     }
 }
---
+++ src/config/device.rs
@@ -0,0 +1,65 @@
+use serde::{Deserialize, Serialize};
+
+use crate::device::Switch;
+
+#[derive(Deserialize, Serialize)]
+pub enum Device {
+    Switch {
+        address: String,
+        receive: Option<String>,
+        transmit: Option<String>,
+        poe: Option<String>,
+    },
+}
+
+impl From<Device> for crate::device::Device {
+    fn from(device: Device) -> Self {
+        (&device).into()
+    }
+}
+
+impl From<&Device> for crate::device::Device {
+    fn from(device: &Device) -> Self {
+        match device {
+            Device::Switch {
+                address,
+                receive,
+                transmit,
+                poe,
+            } => {
+                let labels = format!("instance=\"{address}\"");
+
+                let receive_query = if let Some(receive) = receive {
+                    receive.clone()
+                } else {
+                    format!(
+                        "sum(rate(ifHCInOctets{{{}, ifAlias=~\"(Port|SFP) .*\"}}[1m])) by (ifIndex)",
+                        labels
+                    )
+                };
+
+                let transmit_query = if let Some(transmit) = transmit {
+                    transmit.clone()
+                } else {
+                    format!(
+                        "sum(rate(ifHCOutOctets{{{}, ifAlias=~\"(Port|SFP) .*\"}}[1m])) by (ifIndex)",
+                        labels
+                    )
+                };
+
+                let poe_query = if let Some(poe) = poe {
+                    poe.clone()
+                } else {
+                    format!("unpoller_device_port_poe_amperes{{{}}}", labels)
+                };
+
+                crate::device::Device::Switch(Switch::new(
+                    &labels,
+                    &receive_query,
+                    &transmit_query,
+                    &poe_query,
+                ))
+            }
+        }
+    }
+}
