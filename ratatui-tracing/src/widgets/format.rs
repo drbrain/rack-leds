89d8111ee0b0047dfa39a328b26fcf44032f6ce9
Eric Hodel <drbrain@segment7.net>
2024-11-22 23:01:57 -0800

    Document Format

--- ratatui-tracing/src/widgets/format.rs
+++ ratatui-tracing/src/widgets/format.rs
@@ -5,6 +5,7 @@ use ratatui::{
 
 use crate::widgets::FormatState;
 
+/// Widget to configure [`super::EventLog`] format options
 pub struct Format<'a> {
     block: Option<Block<'a>>,
     cell_highlight_style: Style,
@@ -15,8 +16,44 @@ pub struct Format<'a> {
 }
 
 impl<'a> Format<'a> {
+    /// Wrap the format options with a [`Block`] widget
     pub fn block(mut self, block: Block<'a>) -> Self {
         self.block = Some(block);
+
+        self
+    }
+
+    /// Set the highlight style for the selected format cell
+    pub fn cell_highlight_style(mut self, style: impl Into<Style>) -> Self {
+        self.cell_highlight_style = style.into();
+
+        self
+    }
+
+    /// Set the table header style
+    pub fn header_style(mut self, style: impl Into<Style>) -> Self {
+        self.header_style = style.into();
+
+        self
+    }
+
+    /// Set the selected format row symbol
+    pub fn highlight_symbol(mut self, symbol: impl ToString) -> Self {
+        self.highlight_symbol = symbol.to_string();
+
+        self
+    }
+
+    /// Set the selected table row highlight style
+    pub fn row_highlight_style(mut self, style: impl Into<Style>) -> Self {
+        self.row_highlight_style = style.into();
+
+        self
+    }
+
+    /// Set the table style
+    pub fn table_style(mut self, style: impl Into<Style>) -> Self {
+        self.table_style = style.into();
 
         self
     }
@@ -51,6 +88,11 @@ impl<'a> Format<'a> {
 }
 
 impl<'a> Default for Format<'a> {
+    /// The default `Format` uses the following styles:
+    /// * The selected cell has [`Color::Black`] text on a [`Color::Gray`] background
+    /// * The highlight symbol is `"❯"`
+    /// * The selected row has bold text
+    /// * The table uses the terminal default background color
     fn default() -> Self {
         let header_style = Style::default().bold();
         let cell_highlight_style = Style::default().bold().fg(Color::Black).bg(Color::Gray);
